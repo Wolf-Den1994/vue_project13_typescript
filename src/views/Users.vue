8d1d48557276140dfb6296a9c9f4cc1782f90d2d
Denis <wolf-den@tut.by>
2022-01-03 20:32:15 +0300

    hooks fetch

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="card">
     <h1>Пользователи</h1>
+
+    <user-list></user-list>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import UserList from "@/components/UserList";
+
+export default defineComponent({
+  setup() {
+
+  },
+  components: {UserList}
+})
+</script>
